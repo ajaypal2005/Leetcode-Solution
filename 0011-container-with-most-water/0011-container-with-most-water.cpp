9f3b43846c6b21a896dfa25291e71b6f397e55d3
Ajaypal <ap461719@gmail.com>
2026-07-07 12:06:37 +0530

    Time: 6 ms (7.94%), Space: 63 MB (48.74%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int maxArea = 0;
-        int left = 0;
-        int right = height.size() - 1;
+        int maxArea = 0;                 // Variable to store the maximum area found
+        int left = 0;                    // Left pointer starts at the beginning
+        int right = height.size() - 1;   // Right pointer starts at the end
 
+        // Use two-pointer approach to maximize area
         while (left < right) {
+            // Calculate area formed between left and right lines
+            // Width = (right - left)
+            // Height = min(height[left], height[right]) because water is limited by the shorter line
             maxArea = max(maxArea, (right - left) * min(height[left], height[right]));
 
+            // Move the pointer pointing to the shorter line inward
+            // This is because moving the taller line won't help increase area (height is limited by shorter line)
             if (height[left] < height[right]) {
-                left++;
+                left++;   // Move left pointer forward
             } else {
-                right--;
+                right--;  // Move right pointer backward
             }
         }
 
-        return maxArea;        
+        return maxArea;   // Return the maximum area found
     }
 };
