a2854b127772c3db6273fd9c29e8dec374e7af06
Ajaypal <ap461719@gmail.com>
2026-08-02 21:28:27 +0530

    Time: 0 ms (100%), Space: 9.8 MB (26.35%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t)
+     {
+     vector<int> arr(256,0) ;
+     for(int i = 0; i<s.size(); i++)
+     {
+        arr[s[i]]++;
+     } 
+     
+     for(int i = 0; i<t.size(); i++)
+     {
+        arr[t[i]]--;
+     } 
+     
+     for(int i = 0; i<256; i++){
+        if(arr[i]!=0) return false;
+     } 
+    return true;
+    }
+};
